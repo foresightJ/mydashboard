f654ca849ea9043cd5d4ea218373b5afd517f0db
John U. Amah <84844009+foresightJ@users.noreply.github.com>
2022-06-04 09:01:07 -0400

    added Ecommerce and  Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
-import React from 'react';
-// mount tailwind 
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// icon  library
+import { FiSettings } from 'react-icons/fi';
+
+// failed to cmpile TooltipComponent
+// import { TooltipComponent } from ' @syncfusion/ej2-react-popups';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+// mount tailwind
 import './App.css';
 
 const App = () => {
-	return <h1 className='underline text-3xl'>App</h1>;
+	const activeMenu = true;
+	return (
+		<div className='underline text-3xl'>
+			<BrowserRouter>
+				<div className='flex relative dark:bg-main-dark-bg'>
+					<div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+						<TooltipComponent content='Settings' position='Top'>
+							<button
+								type='button'
+								className='text-3xl p-3 hover: drop-shadow-xl hover:bg-light-grey text-white'
+								style={{ background: 'blue', borderRadius: '50%' }}
+							>
+								<FiSettings />
+							</button>
+						</TooltipComponent>
+					</div>
+					{activeMenu ? (
+						<div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+							Sidebar
+						</div>
+					) : (
+						<div className='w-0 dark:bg-secondary-dark-bg'> Sidebar w-0</div>
+					)}
+					<div
+						className={`dark:bg-main-bg bg-main-bg main-h-screen md:ml-72 w-full ${
+							activeMenu ? 'md:ml-72' : 'flex-2'
+						}`}
+					>
+						<div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+							Navbar
+						</div>
+					</div>
+					<div>
+						<Routes>
+							{/* Dashboard */}
+							<Route path='/' element='ECommerece' />
+							<Route path='/ecommerce' element='ECommerece' />
+
+							{/* Pages */}
+							<Route path='/' element='ECommerece' />
+							<Route path='/ecommerce' element='ECommerece' />
+						</Routes>
+					</div>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
